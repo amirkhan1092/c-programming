dc52cb76ee61692025002a9973979eef292942f5
amirkhan1092 <amirkhan1092@gmail.com>
2020-09-28 00:09:37 +0530

    initial commit

---
+++ sum_of_numbers.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+// for fix values
+// void main()
+// {
+//     int a, b, c;
+//     a = 10;
+//     b = 20;
+//     c = a + b;
+//     printf("Sum of two value is %d", c);
+// }
+
+// values from user
+void main()
+{
+    
+    int a, b, c;
+    // a = 19
+    printf("Enter value of a: \n");
+    scanf("%d",&a);
+    printf("Enter value of b: \n");
+    scanf("%d",&b);
+
+    c = a + b;
+
+    printf("The sum of two number is %s");
+
+}
