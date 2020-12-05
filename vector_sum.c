89e7637a9059b0d18a9fe896101b5333b799c6fd
amirkhan1092 <amirkhan1092@gmail.com>
2020-12-05 23:34:15 +0530

    vector sum without loop

--- vector_sum.c
+++ vector_sum.c
@@ -1,16 +1,35 @@
 #include<stdio.h>
 
+// void main()
+// {
+//     int n;
+//     int sum=0;
+//     scanf("%d", &n);
+ 
+//     while (n>0)
+//     {
+//         sum += n%10;
+//         n /= 10;
+
+//     }
+//     printf("Sum is %d", sum);
+// }
+
 void main()
 {
-    int n;
-    int sum=0;
+    int n, sum=0;
     scanf("%d", &n);
- 
-    while (n>0)
-    {
-        sum += n%10;
-        n /= 10;
+    sum += n%10;
+    n /= 10;
+    sum += n%10;
+    n /= 10;
+    sum += n%10;
+    n /= 10;
+    sum += n%10;
+    n /= 10;
+    sum += n%10;
+    n /= 10;
+    sum += n%10;
 
-    }
-    printf("Sum is %d", sum);
+    printf("%d", sum);    
 }
