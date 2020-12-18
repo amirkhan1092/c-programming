3554e066baba8194d2116e25895b80d5bf645110
amirkhan1092 <amirkhan1092@gmail.com>
2020-12-18 15:05:48 +0530

    new topic

---
+++ test_file.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+// void main()
+// {
+//     int a=2,p;
+//     p= 1==2!=3;
+//     printf("value of p %d\n",p);
+//     printf("Value of a %d", a);
+// }
+
+int main()
+{
+    int x=3, y=4, z;
+    z = x-y*4*4/2;
+    printf("%d", z);
+    return 0;
+
+}
---
+++ width_input.c
@@ -0,0 +1,13 @@
+// input stream width with  scanf()
+
+#include<stdio.h>
+
+void main()
+{
+    printf("This is first line\n");
+    printf("Kaustubh Dubey");
+   // printf("\b\b\b\bVishal");
+    printf("\rVinay");
+    
+}
+
