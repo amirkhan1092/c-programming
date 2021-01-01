3fa46fb755b87465e836049b0d88ddbb31438021
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-01 10:27:16 +0530

    01-Jan

---
+++ printf_test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+void main()
+{
+    int i=2;
+    if(i>1)
+    {
+        printf("hello world!\n");
+        if(i==2)
+            break;
+        printf("C-Programming");    
+    }
+}
