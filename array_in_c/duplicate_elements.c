f91880c84410acd2a894c037a7630ca42a5eaf0a
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-18 23:23:22 +0530

    for compile time values

--- array_in_c/duplicate_elements.c
+++ array_in_c/duplicate_elements.c
@@ -1,22 +1,23 @@
 #include<stdio.h>
 void main()
 {
-    int arr[10];
+    int arr[10] = {2, 43, 43, 3, 22, 43, 2, 4, 54, 3};
     // unsigned int count;
-    for(int i=0; i<10; i++)
-    {   
-        printf("Enter the values at %d Interger ", i);
-        scanf("%d", &arr[i]);
-    }
+    // for(int i=0; i<10; i++)
+    // {   
+    //     printf("Enter the values at %d Interger ", i);
+    //     scanf("%d", &arr[i]);
+    // }
 
     for (int i = 0; i < 10; i++)
     {   
-        for (int j = 0; i < 10; i++)
+        for (int j = 0; j < 10; j++)
         {
-            
-                if(arr[i] == arr[j] && i!=j)
-                    printf("%d", arr[i]);
+                if((arr[i] == arr[j]) && (i!=j))
+                {
+                    printf("\n%d\n", arr[i]);
                     break;
+                }
         }
         
     }
