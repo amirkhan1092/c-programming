adbb735a660afdcf5af59bbf4d4b887559649cf0
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-22 11:57:47 +0530

    22-jan

---
+++ array_in_c/bubble_sort.c
@@ -0,0 +1,38 @@
+
+// program to insert value at given position 
+
+#include<stdio.h>
+#define max 100
+void main()
+{
+    int arr[max];
+    int N, i, j;
+    printf("Enter the number of elements :- ");
+    scanf("%d", &N);
+    int tmp;
+    for ( i = 0; i < N; i++)
+        scanf("%d", &arr[i]);
+    
+    for(i = 0; i < N-1; i++)
+    {
+        for(j=0; j<N-1-i;j++ )
+        {
+            if(arr[j] > arr[j+1])
+            {
+                tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+
+            }
+
+        }
+
+    }
+    printf("\nSorted elements are \n");
+    for(i=0; i<N; i++)
+        printf("%d ", arr[i]);
+
+    
+
+
+}
