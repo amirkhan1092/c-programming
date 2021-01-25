1fb60f5b7dff25f6c6cde61850e57036e69d37fa
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-25 15:07:49 +0530

    sum of matrix

--- array_in_c/matrix.c
+++ array_in_c/matrix.c
@@ -8,26 +8,58 @@
 
 void main()
 {
-    int arr[100][100];
-    int i, j, r, c;
-    printf("Enter the dimension RXC: ");
-    scanf("%dX%d", &r, &c);
-    for(i=0; i<r; i++)
-    {   for(j=0; j<c; j++)
+    int arr1[100][100], arr2[100][100], arr[100][100];
+    int i, j, r1, c1, r2, c2;
+
+    printf("Enter the dimension for matrix1 RXC: ");
+    scanf("%dX%d", &r1, &c1);
+    for(i=0; i<r1; i++)
+    {   for(j=0; j<c1; j++)
         {   
             printf("Enter the dimension [%d]X[%d]: ",i, j );
-            scanf("%d", &arr[i][j]);
+            scanf("%d", &arr1[i][j]);
         }
     }
-
-    // output section
-    printf("\nOutput Matrix or 2D Array\n");
-    for(i=0; i<r; i++)
+    printf("Enter the dimension for matrix2 RXC: ");
+    scanf("%dX%d", &r2, &c2);
+    for(i=0; i<r2; i++)
+    {   for(j=0; j<c2; j++)
+        {   
+            printf("Enter the dimension [%d]X[%d]: ",i, j );
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+    // logic section 
+    if(r1 == r2 && c1 == c2)
     {
-        for(j=0; j<c; j++)
+        for(i=0; i<r1; i++)
+            for(j=0; j<c1; j++)
+                arr[i][j] = arr1[i][j] + arr2[i][j];
+    // output section
+    printf("\nMatrix1\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
+            printf("%d\t", arr1[i][j]);
+        printf("\n");
+    }
+    printf("\nMatrix2\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
+            printf("%d\t", arr2[i][j]);
+        printf("\n");
+    }
+    printf("\nOutput Sum of Matrix or 2D Array\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
             printf("%d\t", arr[i][j]);
         printf("\n");
     }
+    }
+    else   
+        printf("Dimension not valid for operation ");
 
 }
 
