76f1b1e14bfa0706fceaf9a0f0ac745c0d18143e
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-25 15:27:13 +0530

    multiply of two matrix

---
+++ array_in_c/multiply_of_matrix.c
@@ -0,0 +1,77 @@
+
+// 3 53 2
+// 5 77 0
+// matrix in C-Programming 
+// 2D Array
+
+#include<stdio.h>
+
+void main()
+{
+    int arr1[100][100], arr2[100][100], arr[100][100];
+    int i, j, r1, c1, r2, c2;
+
+    printf("Enter the dimension for matrix1 RXC: ");
+    scanf("%dX%d", &r1, &c1);
+    for(i=0; i<r1; i++)
+    {   for(j=0; j<c1; j++)
+        {   
+            printf("Enter the dimension [%d]X[%d]: ",i, j );
+            scanf("%d", &arr1[i][j]);
+        }
+    }
+    printf("Enter the dimension for matrix2 RXC: ");
+    scanf("%dX%d", &r2, &c2);
+    for(i=0; i<r2; i++)
+    {   for(j=0; j<c2; j++)
+        {   
+            printf("Enter the dimension [%d]X[%d]: ",i, j );
+            scanf("%d", &arr2[i][j]);
+        }
+    }
+    // logic section 
+    if(c1 == r2)
+    {
+        for(i=0; i<r1; i++)
+            for ( j = 0; j < c2; j++)
+                arr[i][j] = 0;
+
+        for(i=0; i<r1; i++)
+            for(j=0; j<c2; j++)
+                    for(int k=0; k<c1; k++)
+                        arr[i][j] += arr1[i][k] * arr2[k][j];
+        
+            
+      
+        
+
+
+    // output section
+    printf("\nMatrix1\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
+            printf("%d\t", arr1[i][j]);
+        printf("\n");
+    }
+    printf("\nMatrix2\n");
+    for(i=0; i<r2; i++)
+    {
+        for(j=0; j<c2; j++)
+            printf("%d\t", arr2[i][j]);
+        printf("\n");
+    }
+    printf("\nOutput Sum of Matrix or 2D Array\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c2; j++)
+            printf("%d\t", arr[i][j]);
+        printf("\n");
+    }
+    }
+    else   
+        printf("Dimension not valid for operation ");
+
+}
+
+
