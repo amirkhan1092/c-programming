a6208b7ae52450984ab0f3a3f8bfcd2ccf1fbcba
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-25 13:38:25 +0530

    25-jan

--- array_in_c/matrix.c
+++ array_in_c/matrix.c
@@ -1,33 +1,59 @@
-// 23 54 75
-// 33 56 65
+// matrix in C-Programming 
+// 2D array 
+
+// 3 53 2
+// 4 44 0
 
 
 #include<stdio.h>
 
 void main()
 {
-    // int arr[2][3] = {{3, 5, 3}, {5, 2, 76}};
-    // int matrix[2][3] = {3, 4, 55, 76, 22, 0};
-    int mat[100][100];
-    int r, c, i, j;
-    printf("Enter the dimension R C ");
-    scanf("%d %d", &r, &c);
-    for(i=0; i<r; i++)
-    {   for(j=0; j<c; j++)
-        {
-            printf("Enter the element at [%d]X[%d]", i+1, j+1);
-            scanf("%d", &mat[i][j]); 
+    int arr1[100][100], arr2[100][100], arr[100][100];
+    int i, j, r1, c1, r2, c2;
+    printf("Enter the row and column for matrix1 RXC:- ");
+    scanf("%dX%d", &r1, &c1);
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
+        {   
+            printf("Enter the element at [%d]X[%d]: ", i, j);
+            scanf("%d", &arr1[i][j]);
         }
     }
-    
-    // output section
-    printf("\nOutput Matrix \n");
-    for(i=0; i<r; i++)
+    printf("Enter the row and column for matrix2 RXC:- ");
+    scanf("%dX%d", &r2, &c2);
+    for(i=0; i<r2; i++)
     {
-        for(j=0; j<c; j++)
-            printf("%d\t", mat[i][j]);
-        printf("\n");
+        for(j=0; j<c2; j++)
+        {   
+            printf("Enter the element at [%d]X[%d]: ", i, j);
+            scanf("%d", &arr2[i][j]);
+        }
     }
 
+    // logic section 
+
+    if(r1 == r2 && c1 == c2)
+    {
+
+        for(i=0; i<r1; i++)
+            for(j=0; j<c1; j++)
+                arr[i][j] = arr1[i][j] + arr2[i][j];
+
+    // output section 
+    printf("\nOutput Matrix or 2D Array\n");
+    for(i=0; i<r1; i++)
+    {
+        for(j=0; j<c1; j++)
+            printf("%d\t", arr[i][j]);
+        printf("\n");    
+
+    }
+    }
+    else
+        printf("Not valid dimensions ");
 
 }
+
+
