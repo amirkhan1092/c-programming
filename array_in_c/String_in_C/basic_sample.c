0c89d979c1d4df1c763e4da23e8313325708522b
amirkhan1092 <amirkhan1092@gmail.com>
2021-01-29 20:05:23 +0530

    29-Jan

--- array_in_c/String_in_C/basic_sample.c
+++ array_in_c/String_in_C/basic_sample.c
@@ -4,8 +4,32 @@
 
 void main()
 {
-    char arr[100] = "Am\0ir";
+    char arr[3] = "GLA";
 
-    printf("%s", arr);
+    int count=0;
+
+    while(arr[count++] != '\0');
+
+    // for(count=0; count<5; count++)
+    // {
+    //     printf("%d ", arr[count]);
+    // }
+    
+
+    printf("length of String %d", count-1);
+/*
+    test case1
+    input
+    "RAM PRATAP"
+    output
+    R : 2
+    A : 3
+    M : 1
+      : 1
+    P : 2
+    T : 1  
+
+
+*/
 
 }
---
+++ array_in_c/String_in_C/string_basics.c
@@ -0,0 +1,40 @@
+// string 
+// sequence of char 
+
+#include<stdio.h>
+
+void main()
+{
+    char st[100]; // declaration of string 
+    // int N, num, i;
+    // N = 15;
+    int len=0;
+    printf("Enter the String: ");
+ 
+    // scanf("%s", st);  // read single word
+
+    // scanf("%[^\n]s", st);  // single line full except \n 
+
+    gets(st);
+
+    while(st[len++] != '\0');
+    for(len=0; st[len] != '\0'; len++ );
+
+
+
+    printf("Welcome, %s\nlength is %d\n", st, len-1);
+    // puts(st);
+/*
+   test case0
+   input 
+   RAM BABU
+   output 
+   R : 1
+   A : 2
+   M : 1
+     : 1
+   B : 2
+   U : 1   
+
+*/
+}
